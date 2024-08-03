8a3ac4b72038a3b523ec8bb1be4ef4897c2689a5
jaluarjun442 <160036840+jaluarjun442@users.noreply.github.com>
2024-08-04 01:41:17 +0530

    favicon added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@ use App\Helpers\Helper;
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="{{ asset('favi.png') }}" type="image/x-icon">
     @yield('title')
     @yield('description')
     @yield('schema')
