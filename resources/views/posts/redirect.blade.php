9b994c7dea145897700ab03f71d7da396ea8135b
jaluarjun442 <160036840+jaluarjun442@users.noreply.github.com>
2024-09-14 23:20:44 +0530

    ad redirection handled

--- resources/views/posts/redirect.blade.php
+++ resources/views/posts/redirect.blade.php
@@ -7,12 +7,20 @@
     <title>Home</title>
     <meta name="robots" content="noindex, nofollow">
 
-    <meta http-equiv="refresh" content="{{$ad_redirect_seconds}};url={{$redirect_url}}">
+    <!-- <meta http-equiv="refresh" content="{{$ad_redirect_seconds}};url={{$redirect_url}}"> -->
 
 </head>
 
 <body>
     <p>please wait...</p>
+    <input type="hidden" id="ad_redirect_seconds" name="ad_redirect_seconds" value="{{$ad_redirect_seconds}}" />
+    <script>
+        var ad_redirect_seconds = document.getElementById('ad_redirect_seconds').value;
+        console.log('ad_redirect_seconds',ad_redirect_seconds);
+        setTimeout(function() {
+            window.location.href = '{{$redirect_url}}';
+        }, ad_redirect_seconds);
+    </script>
 </body>
 
 </html>
